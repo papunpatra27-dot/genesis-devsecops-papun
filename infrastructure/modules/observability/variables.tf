bca767c4402d247bdec5fdceed9f1bb1e5ec594b
admin <papunpatra27@gmail.com>
2026-06-11 02:47:24 +0530

    feat(ec2withk3s-AWS): Setup of devsecops ec2 with k3s

--- infrastructure/modules/observability/variables.tf
+++ infrastructure/modules/observability/variables.tf
@@ -1,6 +1,9 @@
 variable "project"           { type = string }
 variable "environment"       { type = string }
-variable "log_retention_days" { type = number; default = 30 }
+variable "log_retention_days" {
+  type = number
+  default = 30
+}
 variable "alarm_email"       { type = string }
 variable "ec2_instance_id"   { type = string }
 
