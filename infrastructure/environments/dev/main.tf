9bef65bbd50d9fc33b1139d7d6dc22939a9ab752
admin <papunpatra27@gmail.com>
2026-06-11 02:49:54 +0530

    feat(ec2withk3s-AWS): Setup of devsecops ec2 with k3s

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -9,7 +9,10 @@ terraform {
   required_version = ">= 1.7.0"
 
   required_providers {
-    aws = { source = "hashicorp/aws"; version = "~> 5.50" }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.50"
+    }
   }
 
   backend "s3" {}
